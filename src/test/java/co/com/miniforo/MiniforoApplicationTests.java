aa1a66d59bd53891c4650c32cfdd27440b624a95
Alejandro <79292993+alejandromagno1@users.noreply.github.com>
2021-11-11 19:11:16 -0500

    Primer cargue del proyecto

---
+++ src/test/java/co/com/miniforo/MiniforoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.miniforo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniforoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
